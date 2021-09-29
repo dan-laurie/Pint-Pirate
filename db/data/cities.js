1d4d9f547c48817b5ef1ec27b22aabc1e590bcc8
Kwasi <cubesprince@gmail.com>
2021-09-29 10:38:24 +0100

    adding seeding data

--- db/data/cities.js
+++ db/data/cities.js
@@ -1,15 +1,236 @@
 export default [
   {
-    name: 'London',
-    bio: 'The capital',
-    image: 'google',
+    name: 'Ashford',
+    bio: 'Ashford is a town in the county of Kent, England. It lies on the River Great Stour at the southern or scarp edge of the North Downs, about 61 miles southeast of central London and 15.3 miles northwest of Folkestone by road.',
+    image: 'https://i.imgur.com/URWTfmN.jpg',
     pint: {
-      name: 'beer',
-      price: 3.56,
-      abv: 4.5,
-      image: 'beer',
-      bio: 'a beer',
-      locations: ['Red Lion', 'The Globe']
+      name: 'San Miguel',
+      price: 4.60,
+      abv: 5.0,
+      image: 'https://i.imgur.com/j3ccmla.png',
+      bio: 'San Miguel is a pilsner-style lager, golden yellow in colour with reflections of old gold. On the nose it offers hints of smoke against a background of grains. On the palate it is balanced, toasted, of medium bitterness with a prevailing subtle toasting of the barley. ',
+      locations: ['The Black Horse', 'The Bowl Inn', 'The Little Black Dog']
+    }
+  },
+  {
+    name: 'Brighton',
+    bio: 'Brighton is a seaside resort and one of the two main areas of the city of Brighton and Hove. Located on the southern coast of England, in the county of East Sussex, it is 47 miles south of London. Archaeological evidence of settlement in the area dates back to the Bronze Age, Roman and Anglo-Saxon periods.',
+    image: 'https://i.imgur.com/0QlnqrO.jpg',
+    pint: {
+      name: 'Grolsch Premium Pilsner',
+      price: 3.30,
+      abv: 4.0,
+      image: 'https://i.imgur.com/uTtwFru.png',
+      bio: 'Grolsch has been brewed in the Netherlands for 400 years. Our Pilsner is brewed for refreshment and greater depth of flavour. Perfectly blending malted barley and 2 hops - one for aroma, one for bitterness - creates our famous taste',
+      locations: ['The Kings Arms', 'The Cricketers']
+    }
+  },
+  {
+    name: 'Bedford',
+    bio: 'Bedford is a large market town in Bedfordshire, England. At the 2011 Census, the population of the Bedford built-up area was 106,940, making it the second-largest settlement in Bedfordshire behind Luton whilst the Borough of Bedford had a population of 157,479.',
+    image: 'https://i.imgur.com/QyWUIN1.jpg',
+    pint: {
+      name: 'Carlsberg',
+      price: 3.00,
+      abv: 4.8,
+      image: 'https://i.imgur.com/FpmZVIh.png',
+      bio: 'The brew has a full mouthfeel, a great smoothness and depth of flavour, with crisp bitterness and a distinctive hop aroma in every can. ',
+      locations: ['Toby Carvery Goldington', 'The Anchor', ' The Stone Jug']
+    }
+  },
+  {
+    name: 'Canterbury',
+    bio: 'Canterbury, a cathedral city in southeast England, was a pilgrimage site in the Middle Ages. Ancient walls, originally built by the Romans, encircle its medieval centre with cobbled streets and timber-framed houses. Canterbury Cathedral, founded 597 A.D., is the headquarters of the Church of England and Anglican Communion, incorporating Gothic and Romanesque elements in its stone carvings and stained-glass windows.',
+    image: 'https://i.imgur.com/Eb3VPhU.jpg',
+    pint: {
+      name: 'Stella',
+      price: 3.40,
+      abv: 5.0,
+      image: 'https://i.imgur.com/GtMA6xQ.png',
+      bio: 'True to the time-honored recipe, Stella Artois is crafted with three ingredients–saaz hops, malted barley, and water. It has a wonderful hop aroma, well-balanced malt sweetness, crisp hop bitterness and a soft dry finish. It is the perfect beer to enjoy with food and friends.',
+      locations: ['Alberrys', 'Bishops Finger', 'The Monument']
+    }
+  },
+  {
+    name: 'Cambridge',
+    bio: 'Cambridge is a city on the River Cam in eastern England, home to the prestigious University of Cambridge, dating to 1209. University colleges include King’s, famed for its choir and towering Gothic chapel, as well as Trinity, founded by Henry VIII, and St John’s, with its 16th-century Great Gate. University museums have exhibits on archaeology and anthropology, polar exploration, the history of science and zoology.',
+    image: 'https://i.imgur.com/xJ4XgLd.jpg',
+    pint: {
+      name: 'Guiness',
+      price: 3.50,
+      abv: 4.2,
+      image: 'https://i.imgur.com/bmrr3wW.png',
+      bio: 'Guinness has a malty sweetness and a hoppy bitterness, with notes of coffee and chocolate. A roasted flavor also comes through, courtesy of the roasted unmalted barley that goes into its brewing.',
+      locations: ['1815 The Union Bar', 'The Courtyard']
+    }
+  },
+  {
+    name: 'Chelmsford',
+    bio: 'Chelmsford is an English city northeast of London. Hylands House, built in 1730, has restored interiors and hundreds of acres of parkland. The medieval Chelmsford Cathedral features stained glass and a colourful ceiling. ',
+    image: 'https://i.imgur.com/LMTwYyW.jpg',
+    pint: {
+      name: 'Fosters',
+      price: 2.60,
+      abv: 4.0,
+      image: 'https://i.imgur.com/W1Qe874.png',
+      bio: 'An easy-drinking lager with a delicate fruity hop aroma and a balanced taste, Fosters rolls of the tongue leaving a clean crisp finish. Its balance of subtle fruitiness and vanilla is ideal for the sweeter-toothed lager lover',
+      locations: ['Acanteen', 'The Woolpack', 'Bar Vino', 'The Rayleigh Arms']
+    }
+  },
+  {
+    name: 'Colchester',
+    bio: 'Colchester is a town in the county of Essex, in southeast England. In the Castle Park, Colchester Castle is a huge Norman keep, built on the foundations of a Roman temple. Its museum displays artefacts up to 2,500 years old. Also in the park is Hollytrees Museum, a Georgian townhouse displaying 3 centuries of clothing, clocks and decorative arts.',
+    image: 'https://i.imgur.com/xoA1hRJ.jpg',
+    pint: {
+      name: 'Timothy Taylors Landlord',
+      price: 2.50,
+      abv: 4.3,
+      image: 'https://i.imgur.com/SqsIivs.png',
+      bio: 'Timothy Taylors best known ale is Landlord, a pale ale, 4.3% abv when cask conditioned, and 4.1% when sold filtered in the bottle.Medium to strong strength finish, nice biscuity feel, sharp barley and bread, ending with some leafy spices. ',
+      locations: ['The Swan', 'Two Brews', 'The White Hart', 'Boadicea']
+    }
+  },
+  {
+    name: 'Guildford',
+    bio: 'Guildford is a town in southern England. In the centre is the medieval Guildford Castle, with landscaped gardens and views from its square tower. Guildford House, a 17th-century town home, has a gallery with rotating exhibitions. Southwest, Loseley Park features a 16th-century manor house with a walled garden.',
+    image: 'https://i.imgur.com/TzWpFwS.jpg',
+    pint: {
+      name: 'San Miguel',
+      price: 3.40,
+      abv: 5.0,
+      image: 'https://i.imgur.com/j3ccmla.png',
+      bio: 'San Miguel is a pilsner-style lager, golden yellow in colour with reflections of old gold. On the nose it offers hints of smoke against a background of grains. On the palate it is balanced, toasted, of medium bitterness with a prevailing subtle toasting of the barley.',
+      locations: ['Wooden Bridge', 'Anchor & Horseshoes']
+    }
+  },
+  {
+    name: 'Ipswich',
+    bio: 'Ipswich is a town in Suffolk, a county in eastern England. Set on the River Orwell, its waterfront is lined with cafes, galleries and shops, and the 19th-century Old Custom House, which recalls the city’s maritime history.',
+    image: 'https://i.imgur.com/Fm7yQ5S.jpg',
+    pint: {
+      name: 'Carlsberg',
+      price: 2.00,
+      abv: 4.8,
+      image: 'https://i.imgur.com/FpmZVIh.png',
+      bio: 'The brew has a full mouthfeel, a great smoothness and depth of flavour, with crisp bitterness and a distinctive hop aroma in every can.',
+      locations: ['The Thomas Wolsey', 'The Steamboat Tavern', 'Royal George']
+    }
+  },
+  {
+    name: 'London',
+    bio: 'London, the capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times. At its centre stand the imposing Houses of Parliament, the iconic ‘Big Ben’ clock tower and Westminster Abbey, site of British monarch coronations. Across the Thames River, the London Eye observation wheel provides panoramic views of the South Bank cultural complex, and the entire city.',
+    image: 'https://i.imgur.com/Mc508Yl.jpg',
+    pint: {
+      name: 'Carlsberg',
+      price: 1.54,
+      abv: 4.8,
+      image: 'https://i.imgur.com/FpmZVIh.png',
+      bio: 'The brew has a full mouthfeel, a great smoothness and depth of flavour, with crisp bitterness and a distinctive hop aroma in every can.',
+      locations: ['The Champion', 'Adam & Eve', 'Ridley Road Market Bar', 'The Citte of Yorke', 'The Social']
+    }
+  },
+  {
+    name: 'Luton',
+    bio: 'Luton is a large town, borough and unitary authority situated in Bedfordshire, south east England, but in the East of England region for administrative purposes. It has a population of 213,052 and is one of the most populous towns without city status in the United Kingdom.',
+    image: 'https://i.imgur.com/3cWhi6h.jpg',
+    pint: {
+      name: 'Carlsberg',
+      price: 1.99,
+      abv: 4.8,
+      image: 'https://i.imgur.com/FpmZVIh.png',
+      bio: 'The brew has a full mouthfeel, a great smoothness and depth of flavour, with crisp bitterness and a distinctive hop aroma in every can.',
+      locations: ['Star', 'Wigmore Arms', 'Frog & Rhubarb']
+    }
+  },
+  {
+    name: 'Maidstone',
+    bio: 'Maidstone is the largest town in Kent, England, of which it is the county town. Maidstone is historically important and lies 32 miles east-south-east of London. The River Medway runs through the centre of the town, linking it with Rochester and the Thames Estuary.',
+    image: 'https://i.imgur.com/QlaXJDH.jpg',
+    pint: {
+      name: 'San Miguel',
+      price: 3.40,
+      abv: 5.0,
+      image: 'https://i.imgur.com/j3ccmla.png',
+      bio: 'San Miguel is a pilsner-style lager, golden yellow in colour with reflections of old gold. On the nose it offers hints of smoke against a background of grains. On the palate it is balanced, toasted, of medium bitterness with a prevailing subtle toasting of the barley.',
+      locations: ['Market House', 'The Druids Arms', 'The Society Rooms']
+    }
+  },
+  {
+    name: 'Margate',
+    bio: 'Margate is a town on England’s southeast coast. Its known for its sandy beach. Near the Harbour Arm stone pier, the modern Turner Contemporary art gallery has rotating exhibitions. Dreamland Margate is an amusement park with vintage rides. Millions of seashells decorate the Shell Grottos underground passages.',
+    image: 'https://i.imgur.com/I82N3zQ.jpg',
+    pint: {
+      name: 'San Miguel',
+      price: 4.10,
+      abv: 5.0,
+      image: 'https://i.imgur.com/j3ccmla.png',
+      bio: 'San Miguel is a pilsner-style lager, golden yellow in colour with reflections of old gold. On the nose it offers hints of smoke against a background of grains. On the palate it is balanced, toasted, of medium bitterness with a prevailing subtle toasting of the barley. ',
+      locations: ['Charlie', 'J D Wetherspoon']
+    }
+  },
+  {
+    name: 'Milton Keynes',
+    bio: 'Milton Keynes is the largest town in Buckinghamshire, England, 50 miles north-west of London. At the 2011 Census, the population of its urban area was almost 230,000. The River Great Ouse forms its northern boundary; a tributary, the River Ouzel, meanders through its linear parks and balancing lakes.',
+    image: 'https://i.imgur.com/CeCSew9.jpg',
+    pint: {
+      name: 'Fosters',
+      price: 2.90,
+      abv: 4.0,
+      image: 'https://i.imgur.com/W1Qe874.png',
+      bio: 'An easy-drinking lager with a delicate fruity hop aroma and a balanced taste, Fosters rolls of the tongue leaving a clean crisp finish. Its balance of subtle fruitiness and vanilla is ideal for the sweeter-toothed lager lover.',
+      locations: ['Wetherspoons', 'Midsummer Tap', 'BrewDog Milton Keynes']
+    }
+  },
+  {
+    name: 'Norwich',
+    bio: 'Norwich is a city in Englands Norfolk County. Northeast of the centre, medieval Norwich Cathedral is a Romanesque building with ornate cloisters. Two imposing gates, St. Ethelberts and Erpingham, lead to the Tombland area, once a Saxon marketplace.',
+    image: 'https://i.imgur.com/X9UYNFb.jpg',
+    pint: {
+      name: 'Fosters',
+      price: 2.80,
+      abv: 4.0,
+      image: 'https://i.imgur.com/W1Qe874.png',
+      bio: 'An easy-drinking lager with a delicate fruity hop aroma and a balanced taste, Fosters rolls of the tongue leaving a clean crisp finish. Its balance of subtle fruitiness and vanilla is ideal for the sweeter-toothed lager lover',
+      locations: ['Bar 11', 'Franks Bar', 'The Fat Cat Pub']
+    }
+  },
+  {
+    name: 'Portsmouth',
+    bio: 'Portsmouth is a port city and naval base on England’s south coast, mostly spread across Portsea Island. It’s known for its maritime heritage and Portsmouth Historic Dockyard. ',
+    image: 'https://i.imgur.com/to2yDp4.jpg',
+    pint: {
+      name: 'Fosters',
+      price: 3.00,
+      abv: 4.0,
+      image: 'https://i.imgur.com/W1Qe874.png',
+      bio: 'An easy-drinking lager with a delicate fruity hop aroma and a balanced taste, Fosters rolls of the tongue leaving a clean crisp finish. Its balance of subtle fruitiness and vanilla is ideal for the sweeter-toothed lager lover',
+      locations: ['Ship Anson', 'The Hole In The Wall']
+    }
+  },
+  {
+    name: 'Southend-on-sea',
+    bio: 'Southend-on-Sea is a resort town on the Thames Estuary in Essex, southeast England. The long Southend Pier is serviced by a small train and has a museum at its shore end. Nearby, Adventure Island is an amusement park with fairground rides and water slides.',
+    image: 'https://i.imgur.com/fWr1itT.jpg',
+    pint: {
+      name: 'Grolsch Premium Pilsner',
+      price: 3.30,
+      abv: 4.0,
+      image: 'https://i.imgur.com/uTtwFru.png',
+      bio: 'Grolsch has been brewed in the Netherlands for 400 years. Our Pilsner is brewed for refreshment and greater depth of flavour. Perfectly blending malted barley and 2 hops - one for aroma, one for bitterness - creates our famous taste',
+      locations: ['The Old Walnut Tree', 'The Oakwood', 'Guildford Southend']
+    }
+  },
+  {
+    name: 'Reading',
+    bio: 'Reading is a large town on the Thames and Kennet rivers in southern England. It’s known for the annual Reading Festival, an outdoor rock music event. Shops and riverside restaurants dot the town centre.',
+    image: 'https://i.imgur.com/7LT3Fzt.jpg',
+    pint: {
+      name: 'Stella',
+      price: 2.50,
+      abv: 5.0,
+      image: 'https://i.imgur.com/GtMA6xQ.png',
+      bio: 'True to the time-honored recipe, Stella Artois is crafted with three ingredients–saaz hops, malted barley, and water. It has a wonderful hop aroma, well-balanced malt sweetness, crisp hop bitterness and a soft dry finish. It is the perfect beer to enjoy with food and friends.',
+      locations: ['Seven Red Roses', 'Bull & Chequers', ' The Water Tower']
     }
   }
 ]
