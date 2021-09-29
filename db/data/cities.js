3405b15fe460d32df63a1f82f5e7788ac3515412
Wala Zeidan <walazeidan123456789@gmail.com>
2021-09-29 10:38:33 +0100

    cities

--- db/data/cities.js
+++ db/data/cities.js
@@ -1,15 +1,224 @@
 export default [
   {
-    name: 'London',
-    bio: 'The capital',
-    image: 'google',
+    name: 'Manchester',
+    bio: 'Situated in the North-West of England, Manchester is one of the UKs most dynamic and vibrant citys. With lots to experience from endless museums to endless pubs - youre sure to find something that peaks your interest!',
+    image: 'https://imgur.com/a/1rDD2AO',
     pint: {
-      name: 'beer',
+      name: 'Hydes Bitter',
+      price: 2.00,
+      abv: 3.8,
+      image: 'https://imgur.com/RSJhWm4',
+      bio: 'A classic and highly popular amber coloured session ale. Made using a combination of the finest malts and English hops – Fuggles and Wye Challenger to provide a fruity and spicy palate.',
+      locations: ['Sinclairs Oyster Bar', 'The Friendship Inn', 'The City Arms']
+    }
+  },
+  {
+    name: 'Leeds',
+    bio: 'Famous for its eclectic nightlife and for its many unique subcultures, Leeds is on the UKs most iconic citys for a night out on the town!',
+    image: 'https://imgur.com/vyYqVuL',
+    pint: {
+      name: 'Theakstons',
+      price: 1.90,
+      abv: 4.5,
+      image: 'https://imgur.com/RSJhWm4',
+      bio: 'A potent mix of Summit, El Dorado, Cascade and Chinook hops delivers a hefty zing of spicy citrus, sweet orange, and grapefruit. Generous helpings of Munich malt give balance and a pale gold coloured appearance, all to create the ultimate thirst quencher!',
+      locations: ['Miners Arms']
+    }
+  },
+  {
+    name: 'Sheffield',
+    bio: 'Sheffield is among the UKs most exciting music citys, yielding some of the greatest bands to come out of the 90s and 00s. Located in South Yorkshire with a quaint city centre and a thriving nightlife, come down and see what the fuss is all about!  ',
+    image: 'https://imgur.com/Do1liGN',
+    pint: {
+      name: 'Farmers Blonde',
+      price: 2.00,
+      abv: 4.3,
+      image: 'https://imgur.com/XtkpOFH',
+      bio: 'This award winning, very pale brilliant blonde beer has citrus and summer fruit aromas making it an extremely refreshing and ‘easy drinking’ ale.',
+      locations: ['Nags Head Inn', 'King & Miller']
+    }
+  },
+  {
+    name: 'Bradford',
+    bio: 'Bradford is one of the biggest cities in England by population size, coming in fifth after Sheffield, Leeds, Birmingham and London. Notorious for its wide range of food and drink, Bradford is surely one of the most underrated citys in the UK.',
+    image: 'https://imgur.com/DkQcnbC',
+    pint: {
+      name: 'Greene King IPA',
+      price: 1.89,
+      abv: 4.0,
+      image: 'https://imgur.com/QgcIoXy',
+      bio: ' Carefully selected pale, crystal and black malts join with Pilgrim, Challenger and First Gold Hops make this complex, refreshing ale.',
+      locations: ['The Turls Green']
+    }
+  },
+  {
+    name: 'York',
+    bio: 'Once seen as a charming day trip for those tearoom aficionados of a certain age, today York is a hotspot for tourists, downsizers, families and young professionals alike, artfully combining the old and new.',
+    image: 'https://imgur.com/bEv4PDd',
+    pint: {
+      name: 'Sam Smiths',
       price: 3.56,
       abv: 4.5,
-      image: 'beer',
-      bio: 'a beer',
-      locations: ['Red Lion', 'The Globe']
+      image: 'https://imgur.com/undefined',
+      bio: 'Brewed with well water, best barley malt, yeast and aromatic hops; fermented in ‘stone Yorkshire squares’ to create a full, rounded flavour and after-taste.',
+      locations: ['Samuel Smiths Pubs']
+    }
+  },
+  {
+    name: 'Newcastle',
+    bio: ' Newcastle has everything you would expect of a major European regional capital city: culture, heritage, great night life and fabulous retail.',
+    image: 'https://imgur.com/vZLwpIM',
+    pint: {
+      name: 'Fosters',
+      price: 2.50,
+      abv: 5.0,
+      image: 'https://imgur.com/juRPkMO',
+      bio: 'Fosters is a light, golden lager with a medium malt character. An easy-drinking lager with a delicate fruity hop aroma and a balanced taste, Fosters rolls of the tongue leaving a clean crisp finish.',
+      locations: ['Wetherspoons']
+    }
+  },
+  {
+    name: 'Liverpool',
+    bio: 'Liverpool is known for its influence on music, football and fashion. The Beatles were formed in Liverpool, Everton and Liverpool FC are legendary football clubs and it’s a haven for shopping. It’s also a great city for culture and literature.',
+    image: 'https://imgur.com/cMUM8Bi',
+    pint: {
+      name: 'Carling',
+      price: 1.90,
+      abv: 3.7,
+      image: 'https://imgur.com/L8vbLgW',
+      bio: 'Carling is brewed to have the perfect balance of sweetness and bitterness, giving it that brilliantly refreshing taste from the first sip to the last.',
+      locations: ['Woodys Liverpool', 'Rubber Soul']
+    }
+  },
+  {
+    name: 'Blackpool',
+    bio: 'The UKs favourite beach resort features world-class attractions from a theme park to an aquarium, stunning beaches to the Blackpool Illuminations, a waterpark to a zoo,  wax figure celebrity look-a-likes and the unforgettable Blackpool Tower. These amazing experiences are underpinned by a year-round programme of events and fantastic shows.',
+    image: 'https://imgur.com/YaHvwmW',
+    pint: {
+      name: 'Fosters',
+      price: 1.55,
+      abv: 5.0,
+      image: 'https://imgur.com/juRPkMO',
+      bio: 'Fosters is a light, golden lager with a medium malt character. An easy-drinking lager with a delicate fruity hop aroma and a balanced taste, Fosters rolls of the tongue leaving a clean crisp finish.',
+      locations: ['The Ardwick']
+    }
+  },
+  {
+    name: 'Stoke-on-Trent',
+    bio: 'Stoke-on-Trent is a unique city affectionately known as The Potteries. A creative city, Sheffield is known to be as a place where art, technology and commerce come together through craftsmanship.',
+    image: 'https://imgur.com/AaV5yr2',
+    pint: {
+      name: 'Carling',
+      price: 2.50,
+      abv: 3.7,
+      image: 'https://imgur.com/L8vbLgW',
+      bio: 'Carling is brewed to have the perfect balance of sweetness and bitterness, giving it that brilliantly refreshing taste from the first sip to the last.',
+      locations: ['The White Star', 'Hanley Brewers Fayre']
+    }
+  },
+  {
+    name: 'Leicester',
+    bio: 'A vibrant, diverse and colourful city it is home to world-renowned space research, the National Space Centre, a strong thriving business community and three Universities. With expertise in the food sector, it is home to Walkers crisps, Melton Mowbray Pork Pies and Stilton cheese. ',
+    image: 'https://imgur.com/fDt3oVA',
+    pint: {
+      name: 'Fosters',
+      price: 2.20,
+      abv: 5.0,
+      image: 'https://imgur.com/juRPkMO',
+      bio: 'Fosters is a light, golden lager with a medium malt character. An easy-drinking lager with a delicate fruity hop aroma and a balanced taste, Fosters rolls of the tongue leaving a clean crisp finish.',
+      locations: ['Friary Leicester', 'The High Cross']
+    }
+  },
+  {
+    name: 'Wolverhampton',
+    bio: 'Wolverhampton is a vibrant City, perfectly located in the heart of the West Midlands. Known for its rich cultural diversity and is a thriving centre for arts and entertainment. ',
+    image: 'https://imgur.com/SVv60oq',
+    pint: {
+      name: 'Fosters',
+      price: 2.20,
+      abv: 5.0,
+      image: 'https://imgur.com/juRPkMO',
+      bio: 'Fosters is a light, golden lager with a medium malt character. An easy-drinking lager with a delicate fruity hop aroma and a balanced taste, Fosters rolls of the tongue leaving a clean crisp finish.',
+      locations: ['Hogshead Wolverhampton', 'The George Wallis']
+    }
+  },
+  {
+    name: 'Nottingham',
+    bio: 'A green city with a legendary past, a vibrant culture and a bright future. A creative destination of rebels and pioneers, and the home of Robin Hood. Nottinghamshire is one of the UKs top destinations, and a warm welcome always awaits.',
+    image: 'https://imgur.com/J0fLbVB',
+    pint: {
+      name: 'Fosters',
+      price: 2.70,
+      abv: 5.0,
+      image: 'https://imgur.com/juRPkMO',
+      bio: 'Fosters is a light, golden lager with a medium malt character. An easy-drinking lager with a delicate fruity hop aroma and a balanced taste, Fosters rolls of the tongue leaving a clean crisp finish.',
+      locations: ['Ye Olde Trip To Jerusalem', 'The Cross Keys', 'Canalhouse']
+    }
+  },
+  {
+    name: 'Glasgow',
+    bio: 'Vibrant, edgy and brimming with personality, Scotland’s largest city is a must-see spot for art, culture and cuisine, while its proximity to Loch Lomond & The Trossachs National Park make it perfect for nature lovers too.',
+    image: 'https://imgur.com/ZtVCcbZ',
+    pint: {
+      name: 'Tennents',
+      price: 2.10,
+      abv: 4.0,
+      image: 'https://imgur.com/ND6yc65',
+      bio: 'Tennents Lager is brewed to meticulously high standards, using exceptional ingredients and characterised by its clean taste and refreshingly crisp finish best served ice cold. Having maintained its popularity in the face of changing tastes and fashions, Tennent Lager has also earned widespread acclaim for its staunch commitment to football and music in Scotland.',
+      locations: ['Tennents Bar', 'Drum & Monkey', 'Curlers Rest']
+    }
+  },
+  {
+    name: 'Edinburgh',
+    bio: 'With an abundance of history, top quality attractions and beautiful architecture, Edinburgh is a city that you wont forget in a hurry. Explore the Medieval Old Town and the elegant Georgian New Town, which sit side by side, and visit unique attractions throughout the city on a holiday in Edinburgh.',
+    image: 'https://imgur.com/XmI21m1',
+    pint: {
+      name: 'Tennents',
+      price: 3.10,
+      abv: 4.0,
+      image: 'https://imgur.com/ND6yc65',
+      bio: 'Tennents Lager is brewed to meticulously high standards, using exceptional ingredients and characterised by its clean taste and refreshingly crisp finish best served ice cold. Having maintained its popularity in the face of changing tastes and fashions, Tennent Lager has also earned widespread acclaim for its staunch commitment to football and music in Scotland.',
+      locations: ['The Worlds End', 'Bennets Bar', 'The Bow Bar']
+    }
+  },
+  {
+    name: 'Dundee',
+    bio: 'Scotlands hottest (or at least sunniest!) city, Dundee, is a dynamic and compact city thats becoming a modern wonder. Not only is it a UNESCO City of Design, but its home to the V&A Dundee, the worlds only V&A museum outside of London.',
+    image: 'https://imgur.com/KGecrqe',
+    pint: {
+      name: 'Tennents',
+      price: 2.70,
+      abv: 4.0,
+      image: 'https://imgur.com/ND6yc65',
+      bio: 'Tennents Lager is brewed to meticulously high standards, using exceptional ingredients and characterised by its clean taste and refreshingly crisp finish best served ice cold. Having maintained its popularity in the face of changing tastes and fashions, Tennent Lager has also earned widespread acclaim for its staunch commitment to football and music in Scotland.',
+      locations: ['Trades House', 'Dukes Corner', 'The Phoenix']
+    }
+  },
+  {
+    name: 'Aberdeen',
+    bio: 'Shimmy up to Scotlands third-largest city for its granite-hewn architecture - long hailed as the most beautiful in Europe - city surfing and an eclectic creative scene fuelled by vibrant street art and "Made in Aberdeen" collectives such as Look Inside.',
+    image: 'https://imgur.com/PB1eotJ',
+    pint: {
+      name: 'Innes and Gunn',
+      price: 2.49,
+      abv: 6.6,
+      image: 'https://imgur.com/JvsVKlm',
+      bio: 'This Scottish Golden Beer is cask-matured using single malt whisky casks for unparalleled smoothness.',
+      locations: ['Archibald Simpson']
+    }
+  },
+  {
+    name: 'Iverness',
+    bio: 'Head north for a weekend break or holiday in Inverness! This vibrant city offers all the wonders of the Highlands on its doorstep.',
+    image: 'https://imgur.com/YTUe6i4',
+    pint: {
+      name: 'Tennents',
+      price: 2.60,
+      abv: 4.0,
+      image: 'https://imgur.com/ND6yc65',
+      bio: 'Tennents Lager is brewed to meticulously high standards, using exceptional ingredients and characterised by its clean taste and refreshingly crisp finish best served ice cold. Having maintained its popularity in the face of changing tastes and fashions, Tennent Lager has also earned widespread acclaim for its staunch commitment to football and music in Scotland.',
+      locations: ['The Stag', 'Ma Cmaerons', 'Prince of Wales']
     }
   }
+
 ]
