85cfaadd6823a372636f9f2a1f96d5212274bf48
Wala Zeidan <walazeidan123456789@gmail.com>
2021-10-01 11:16:07 +0100

    search

--- client/src/component/CityList.js
+++ client/src/component/CityList.js
@@ -1,24 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import Search from './Search'
 
 
 const CityList = () => {
   const [ cities, setCities ] = useState([])
   const [sortedCities, setSortedCities] = useState([])
   const [ hasErrors, setHasErrors] = useState(false)
-  const [ filters, setFilters ] = useState({ searchTerm: '' })
   const [ filteredCities, setFilteredCities ] = useState([])
-
-  const [prices, setPrices] = useState([])
-
 
   useEffect(() => {
     const getCities = async () => {
       try {
         const { data } = await axios('/api/cities')
         setCities(data)
-        // console.log(data)
       } catch (error) {
         setHasErrors(true)
       }
@@ -27,43 +23,32 @@ const CityList = () => {
   }, [])
 
   const handleOptions = (e) => {
+    setSortedCities([])
     if (e.target.value === 'lh'){
       const lowCities = cities.sort((a, b) => {
         return ((a.pint.price > b.pint.price) ? 1 : -1 )
       })
       setSortedCities(lowCities)
-      console.log(sortedCities)
+      console.log('lh',sortedCities)
     } else if (e.target.value === 'hl'){
       const highCities = cities.sort((a, b) => {
         return ((a.pint.price < b.pint.price) ? 1 : -1 )
       })
       setSortedCities(highCities)
-      console.log(sortedCities)
+      console.log('hl',sortedCities)
     } else if (e.target.value === 'all') {
       setSortedCities(cities)
-      console.log(sortedCities)
+      console.log('all', cities)
     }
     return
   }
 
-  useEffect(() => {
-    const regexSearch = new RegExp(filters.searchTerm, 'i')
-    setFilteredCities(cities.filter(city => {
-      return regexSearch.test(city.name)
-    }))
-  }, [filters, cities])
-
-  const handleFilterChange = (event) => {
-    const newObj = { ...filters, [event.target.name]: event.target.value }
-    console.log('New Obj', newObj)
-    setFilters(newObj)
-  }
 
   return (
     <div className="beer-page">
       <h2>City List</h2>
       <div className="beer-filter">
-        <input onChange={handleFilterChange} name="searchTerm" value={filters.searchTerm} placeholder='Search City 🔎' className='filter-type'/>
+        <Search cities={cities} setFilteredCities={setFilteredCities} />
         <select name="options" onChange={handleOptions} className='sort'>
           <option value="all" defaultValue>All</option>
           <option value="lh">Low to High</option>
@@ -71,29 +56,39 @@ const CityList = () => {
         </select>
       </div>
       
+      
       <div className="row mt-1">
-        {(filteredCities ? filteredCities : cities).map(city => {
-          return (
-            <div key={city.name} className="cities col-lg-3 mb-4 col-md-6">
-              <Link className="card-link" to={`/api/cities/${city.id}`}>
-                <div className='card'>
+
                   
+        {(filteredCities.length > 0 ? filteredCities.map(city => {
+          return (
+            <div className="cities col-lg-3 mb-4 col-md-6" key={city.id}>
+              <Link className="card-link" to={`/api/cities/${city.id}`}>
+                <div  className='card'> 
                   <h4 className="city-name"value={city.name}>{city.name}</h4>
                   <img className="city-image" src={city.image}></img>
                   <h6>£{(city.pint.price).toFixed(2)}</h6>
-                  
                 </div>
               </Link>
             </div>
           )
-        })}
+        })
           :
-        <>
-          <h1>Sorry, Cannot display</h1>
-        </>
+          (cities ? cities : (sortedCities.length > 0 ? sortedCities : cities)).map(city => {
+            return (
+              <div className="cities col-lg-3 mb-4 col-md-6" key={city.id}>
+                <Link className="card-link" to={`/api/cities/${city.id}`}>
+                  <div  className='card'> 
+                    <h4 className="city-name"value={city.name}>{city.name}</h4>
+                    <img className="city-image" src={city.image}></img>
+                    <h6>£{(city.pint.price).toFixed(2)}</h6>
+                  </div>
+                </Link>
+              </div>
+            )
+          }))}
       </div>
     </div>
   )
-
 }
 export default CityList
---
+++ client/src/component/auth/login.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import axios from 'axios' 
+import { Link, useHistory } from 'react-router-dom'
+
+const Login = () => {
+
+  //history
+  const history = useHistory() //enables us to move pages without refresh
+
+  // for login we only need email and password
+  // const [ formData, setFormData ] = useState({
+  //   email: '',
+  //   password: '',
+  // })
+
+  //here we are updating the state of the form in order to POST this info to API
+  //we spread exisiting state in and simply update with the users inputs
+  //using name="" in JSX is crucial, as this can be used as the 'key'
+  // const handleChange = (event) => {
+  //   const newObj = { ...formData, [event.target.name]: event.target.value }
+  //   setFormData(newObj)
+  // }
+
+  // const setTokenToLocalStorage = (token) => {
+  //   window.localStorage.setItem('token', token) // the token is stored in Local Storage, whoop whoop!
+  //   history.push('/cheeses') //upon successful login, we are re-directed to /cheeses
+  // }
+
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault() //stops page reloading on submit
+  //   try {
+  //     const { data } = await axios.post('https://ga-cheesebored.herokuapp.com/login', formData)
+  //     setTokenToLocalStorage(data.token) //here we dig down and get the JWT! And update that to state so it can be stored
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
+
+  return (
+    <div className="site-wrapper">
+      <div className="login-page">
+        <div className="form-page">
+          <div className="container">
+            <div className="row">
+              <form  className="col-10 offset-1 mt-4 col-md-6 offset-md-3">
+                <h3>Login</h3>
+                <div className="form-field">
+                  <label htmlFor="email">Email</label>
+                  <input type="email" name="email" placeholder="Email" />
+                </div>
+                <div className="form-field">
+                  <label htmlFor="password">Password</label>
+                  <input  type="password" name="password" placeholder="Password" />
+                </div>
+                <button className="btn btn-yellow w-100">Login</button>
+                <p className="no-account">Don&apos;t have an Account? <Link to="/register"><span className="click-here">Click Here</span></Link></p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+export default Login
