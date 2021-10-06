b83e84bdb31783229ad0898953e8a4603a357c60
danlaurie <danlaurie3@gmail.com>
2021-10-06 14:57:30 +0100

    profile

--- client/src/component/profile/Profile.js
+++ client/src/component/profile/Profile.js
@@ -1,12 +1,46 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { getTokenFromLocalStorage } from '../helpers/auth'
+import axios from 'axios'
 
 const Profile = () => {
+
+  const token = getTokenFromLocalStorage()
+
+  const [ details, setDetails ] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios('/api/profile', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        setDetails(data)
+        console.log(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getData()
+  
+  }, [token])
+
+
   return (
     <div className="site-wrapper">
       <div className="beer-page">
         <h1>My Profile</h1>
+        <div className="details">
+          <div className="profile-pic">
+            {/* <img src={details} alt="" /> */}
+          </div>
+          <h3>Username: {details.username}</h3>
+          <h3>Email: {details.email}</h3>
+        </div>
+
       </div>
+    
     </div>
   )
 }
